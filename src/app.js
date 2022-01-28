8b2ed1912e85412e2e76104d9cdc3fafbcab57b1
Zayne Arnold <Zayne.a@gmail.com>
2022-01-28 07:04:39 +0200

    JS system added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-import $ from 'jquery/dist/jquery.min.js';  
-import bootstrap from 'bootstrap/dist/js/bootstrap.min.js';
+// import $ from 'jquery/dist/jquery.js';  
+// import $ from "expose-loader?exposes=$,jQuery!jquery";
+
+// import bootstrap from 'bootstrap/dist/js/bootstrap.min.js';
+import './js/_autoload.js';
+import './js/acc.global.js';
 import './css/main.css';
 import './scss/main.scss';
